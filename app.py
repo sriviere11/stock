960a6c54b5e691bd134770b817c0918994d16d28
Steve Riviere <steveriviere@yahoo.com>
2022-10-17 21:24:22 -0400

    a

--- app.py
+++ app.py
@@ -493,5 +493,5 @@ def ticker(ticker):
 if __name__ == '__main__':
     # Used when running locally only. When deploying to Cloud Run,
     # a webserver process such as Gunicorn will serve the app.
-    app.run(host='localhost', port=8090, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
 
